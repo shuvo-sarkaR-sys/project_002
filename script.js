65a77444087f26784cc53194994282935fa5c48f
nexttog <nexttogitservice@gmail.com>
2025-06-27 12:39:10 +0600

    upd

---
+++ script.js
@@ -0,0 +1,25 @@
+ 
+ 
+  const swiper = new Swiper('.mySwiper', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    // autoplay: {
+    //   delay: 2000,
+    //   disableOnInteraction: false,
+    // },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      1024: { slidesPerView: 4 },
+      768:  { slidesPerView: 2 },
+      0:  { slidesPerView: 1 },
+    }
+  });
+ 
