e1ce1937234edd736ec5645d48ec281d7032981c
nexttog <nexttogitservice@gmail.com>
2025-06-29 09:29:05 +0600

    upd

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
  
   const swiper = new Swiper('.mySwiper', {
     slidesPerView: 4,
+      
     spaceBetween: 20,
     loop: true,
-    // autoplay: {
-    //   delay: 2000,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -22,5 +23,36 @@
       0:  { slidesPerView: 1 },
     }
   });
+
+
+
+
+
+
+  const swiper1 = new Swiper('.swiper2', {
+  slidesPerView: 3,
+    slidesPerGroup: 1,
+      
+    spaceBetween: 30,
+    loop: true,
+    // autoplay: {
+    //   delay: 2000,
+    //   disableOnInteraction: false,
+    // },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+         
+    },
+    breakpoints: {
+      1024: { slidesPerView: 3 },
+      768:  { slidesPerView: 2 },
+      0:  { slidesPerView: 1 },
+    }
+});
 document.getElementById('menu-toggle').addEventListener('click', function () {
   document.getElementById('nav-sidebar').classList.toggle('show'); });
