a11deae9ae15bbbd32ccf9acfa885972502fe94d
nexttog <nexttogitservice@gmail.com>
2025-06-29 19:11:17 +0600

    updated shuvo sarkar

--- script.js
+++ script.js
@@ -54,5 +54,33 @@
       0:  { slidesPerView: 1 },
     }
 });
+
+  const swiper3 = new Swiper('.swiper3', {
+  slidesPerView: 3,
+    slidesPerGroup: 1,
+    
+    spaceBetween: 30,
+    loop: true,
+    // autoplay: {
+    //   delay: 2000,
+    //   disableOnInteraction: false,
+    // },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+         
+    },
+    breakpoints: {
+      1024: { slidesPerView: 3 },
+      768:  { slidesPerView: 2 },
+      0:  { slidesPerView: 1 },
+    }
+});
+
+
 document.getElementById('menu-toggle').addEventListener('click', function () {
   document.getElementById('nav-sidebar').classList.toggle('show'); });
