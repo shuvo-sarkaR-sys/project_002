00650fa2bb4b2ea972dd1e104f7b90fbf8d7eb38
nexttog <nexttogitservice@gmail.com>
2025-06-29 12:09:32 +0600

    updated

--- script.js
+++ script.js
@@ -35,10 +35,10 @@
       
     spaceBetween: 30,
     loop: true,
-    // autoplay: {
-    //   delay: 2000,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
